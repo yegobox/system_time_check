7808e094f452a23f5dc7f968097eb6116e4b5984
Richard Muragijimana <beastar457@gmail.com>
2021-12-21 11:00:49 +0200

    init system time check

---
+++ example/android/app/src/main/java/plugin/flipper/system_time_check_example/MainActivity.java
@@ -0,0 +1,6 @@
+package plugin.flipper.system_time_check_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Classes/SystemTimeCheckPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface SystemTimeCheckPlugin : NSObject<FlutterPlugin>
+@end
